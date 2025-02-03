b8610c23a88814d172a1dc57ef519f3197f0d5ed
dxlliv <hello@dxlliv.dev>
2025-02-03 18:44:58 +0100

    Implement i18n in storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -5,13 +5,28 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import '../app/styles/index.scss';
 
+import { createI18n } from "vue-i18n";
+import messages from 'gridsky-i18n/locales'
+
 setup((app) => {
+    // vuetify
+
     const vuetify = createVuetify({
         components,
         directives,
     })
 
     app.use(vuetify)
+
+    // localization
+
+    const i18n = createI18n({
+        messages,
+        legacy: false,
+        locale: "en"
+    })
+
+    app.use(i18n)
 })
 
 const preview = {
