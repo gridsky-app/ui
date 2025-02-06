d69ffc6c50e2c9811164b8aed658254b8478ff47
dxlliv <hello@dxlliv.dev>
2025-02-06 07:15:16 +0100

    Add icons

---
+++ app/components/Icon/IconGrids.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <Icon name="lucide:grid" />
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ app/components/Icon/IconTheme.vue
@@ -0,0 +1,3 @@
+<template>
+  <Icon name="lucide:swatch-book" :size="24"/>
+</template>
